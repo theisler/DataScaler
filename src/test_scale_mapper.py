deecdf80d0016f6159f6542178871b2a926026ea
theisler <tomook@gmail.com>
2013-10-21 12:24:48 -0600

    Expand test suite

--- src/test_scale_mapper.py
+++ src/test_scale_mapper.py
@@ -84,7 +84,97 @@ class TestScaleMapper(unittest.TestCase):
     args = ['a', 'b', 'c']
     (options, message) = scale_mapper.process_args(args)
     self.assertFalse(scale_mapper.validate_args(options, message))
-    
+
+
+  def test_get_field_type1(self):
+    args = "NTNTUG"
+    for cnt in range(len(args)):
+      self.assertEqual(args[cnt], scale_mapper.get_field_type(cnt, args))
+      
+
+  def test_get_field_type2(self):
+    args = "NT"
+    for cnt in range(-3, -1):
+      self.assertEqual('T', scale_mapper.get_field_type(cnt, args))
+      
+
+  def test_get_field_type3(self):
+    args = "NT"
+    for cnt in range(len(args), len(args)+10):
+      self.assertEqual('T', scale_mapper.get_field_type(cnt, args))
+
+
+  def test_extract_column_data1(self):
+    field_type = 'T'
+    col_in = 'The Rain In Spain]\n'
+    col_out = col_in.strip()
+    self.assertEqual(col_out, scale_mapper.extract_column_data(col_in, field_type))
+
+
+  def test_extract_column_data2(self):
+    field_type = 'T'
+    col_in = 'The Rain\tIn Spain\t'
+    col_out = col_in.strip()
+    self.assertEqual(col_out, scale_mapper.extract_column_data(col_in, field_type))
+
+
+  def test_extract_column_data3(self):
+    field_type = 'T'
+    col_in = 'The Rain\nIn Spain]\t'
+    col_out = col_in.strip()
+    self.assertEqual(col_out, scale_mapper.extract_column_data(col_in, field_type))
+
+
+  def test_extract_column_data4(self):
+    field_type = 'T'
+    col_in = '"The Rain In Spain"'
+    col_out = col_in.strip()
+    self.assertEqual(col_out, scale_mapper.extract_column_data(col_in, field_type))
+
+
+  def test_extract_column_data5(self):
+    field_type = 'N'
+    col_in = '4.5e3'
+    col_out = 4500.0
+    self.assertEqual(col_out, scale_mapper.extract_column_data(col_in, field_type))
+
+
+  def test_extract_column_data6(self):
+    field_type = 'N'
+    col_in = '5'
+    col_out = 5.0
+    self.assertEqual(col_out, scale_mapper.extract_column_data(col_in, field_type))
+
+
+  def test_extract_column_data7(self):
+    field_type = 'T'
+    col_in = '5'
+    col_out = '5'
+    self.assertEqual(col_out, scale_mapper.extract_column_data(col_in, field_type))
+
+
+  def test_extract_column_data8(self):
+    field_type = 'N'
+    col_in = '-8134.314'
+    col_out = -8134.314
+    self.assertEqual(col_out, scale_mapper.extract_column_data(col_in, field_type))
+
+
+  def test_extract_column_data9(self):
+    field_type = 'U'
+    col_in = '-8134.314'
+    col_out = -8134.314
+    self.assertEqual(col_out, scale_mapper.extract_column_data(col_in, field_type))
+
+
+  def test_extract_column_data10(self):
+    field_type = 'U'
+    col_in = '-8134.314ADDD'
+    col_out = '-8134.314ADDD'
+    self.assertEqual(col_out, scale_mapper.extract_column_data(col_in, field_type))
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
