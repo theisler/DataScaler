411e9a8eadd1d9b717ddcfd5dff57573010ab584
theisler <tomook@gmail.com>
2013-10-23 19:17:42 -0600

    More unit tests and new functionality for mapping values

--- src/scale_mapper.py
+++ src/scale_mapper.py
@@ -1,10 +1,118 @@
 #!/usr/bin/python
-import sys
 import csv
+import io
+import sys
+
+
+class ScalingData:
+  def __init__(self):
+    self.min = float(sys.maxint)
+    self.max = float(-sys.maxint)
+    self.text_list = { '':0 }
+    self.scale_type = 'U'
+
+  def get_scale_type(self):
+    return self.scale_type
+
+  def set_scale_type(self, new_scale_type):
+    if new_scale_type in ['T','N']:
+      self.scale_type = new_scale_type
+    else:
+      raise AttributeError('Attribute value not valid: ' + str(new_scale_type))
+
+  def get_min(self):
+    return self.min
+
+  def set_min(self, min_value):
+    if min_value < self.min:
+      self.min = float(min_value)
+    return self.min
+
+  def get_max(self):
+    return self.max
+
+  def set_max(self, max_value):
+    if max_value > self.max:
+      self.max = float(max_value)
+    return self.max
+  
+
+  def add_text_value(self, text_value):
+
+    
+  def scale(self, value):
+    # TODO: Should over-ride as type is set, or create numeric and text sub-classes
+    if self.scale_type == 'T':
+      return self.scale_text(value)
+    elif self.scale_type == 'N':
+      return self.scale_number(value)
+    else:
+      raise AttributeError('Scale type must be initialized')
+    
+
+  def scale_default(self, key):
+    raise AttributeError('Scale type must be initialized')
+
+  
+  def scale_text(self, key):
+    key = str(key)
+    if self.text_list.has_key(key):
+      return self.text_list[key]
+    else:
+      return 0
+
+  def scale_number(self, value):
+    retval = (float(value) - self.min) / (self.max - self.min)
+    return retval
+
+
+  def is_numeric(self, value):
+    retval = False
+    try:
+      f = float(value)
+      retval = True
+    except:
+      retval = False
+
+    return retval
+
+
+class TextScalingData:
+  def __init__(self):
+    self.text_list = {}
+
+  
+  def add_data(self, list_id, value):
+    if not self.text_list.has_key(list_id):
+      self.text_list.update( {list_id: {}} ) # Add a new dictionary as needed
+      
+    # Add the value as a key and an increasing scalar as the value (hence using len)
+    self.text_list[list_id].update(value, len(self.text_list[list_id]))
+
+
+  def get_data(self, list_id, key):
+    retval = ''
+    if self.text_list.has_key(list_id):
+      if self.text_list[list_id].has_key(key):
+        retval = self.text_list[list_id][key]
+    return retval
+  
+
+
+# Global variables
+csv_reader = csv.reader
+csv_writer = csv.writer
+output_buffer = io.BytesIO()
+
 
 def get_csv_reader(input_file):
   csv_reader = csv.reader(input_file, delimiter=',', quotechar='"')
   return csv_reader
+
+
+def get_csv_writer(output_file):
+  csv_writer = csv.writer(output_file, quotechar = '"', quoting=csv.QUOTE_MINIMAL)
+  return csv_writer
 
 
 def process_header_row(row):
@@ -159,18 +267,38 @@ def read_range_file(file_name):
     process_range_file_line(line, results)
 
   f.close()
-    
+  
+  return results
+
+
 def process_range_file_line(line, results):
   line = prepare_range_file_line(line)
-  (record_type, col_name) = extract
+  (record_type, col_name, value) = extract_data_from_line(line)
+  if record_type == 'Text':
+    None
 
 
 def prepare_range_file_line(line):
   retval = line.strip()
   return retval
 
+def format_output_line(row_data):
+  retval = ""
+  return retval
+
+
+def init_io(input_file, output_file):
+  csv_reader = get_csv_reader(input_file)
+  csv_writer = get_csv_writer(output_file)
+  return (csv_reader, csv_writer)
+
   
 def main(argv):
+  global csv_reader
+  global csv_writer
+  global output_buffer
+
+  
   args = sys.argv[1:]
   (options, messages) = process_args(args)
   if not validate_args(options, messages):
@@ -182,7 +310,9 @@ def main(argv):
 
   range_data = read_range_file(range_file_name)
   
-  csv_reader = get_csv_reader(sys.stdin)
+  # Need to explain slight of hand for output_buffer/stdio
+  (csv_reader, csv_writer) = init_io(sys.stdin, sys.stdio)
+  
   cnt_row = 0
   
   try:
@@ -191,8 +321,8 @@ def main(argv):
         headers = process_header_row(row)
       else:
         field_data = process_row(row, field_types)
-        field_data
-        emit_row(sys.stdout(
+        csv_writer.writerow(field_data) # 
+        sys.stdout.write(output_buffer) #
 
       cnt_row += 1
                 
--- src/test_scale_mapper.py
+++ src/test_scale_mapper.py
@@ -1,4 +1,7 @@
+import io
+import csv
 import scale_mapper
+import sys
 import unittest
 
 class TestScaleMapper(unittest.TestCase):
@@ -211,6 +214,140 @@ class TestScaleMapper(unittest.TestCase):
     input_data = ['ABC', '2.4', '4.59', ' -4.3e3\t\n', 'QA', '1.3']
     output_data = ['ABC', 2.4, '4.59', -4300, 'QA', 1.3]
     self.assertEqual(scale_mapper.process_row(input_data, field_types), output_data)
+
+
+  def test_format_output_line1(self):
+    field_data = ['ABC', 2.4, '4.59', -4300, 'QA', 1.3]
+    expected_output = 'ABC,2.4,4.59,-4300,QA,1.3'
+    output_buffer = io.BytesIO() # Essentially, mocking file output
+    csv_writer = scale_mapper.get_csv_writer(output_buffer)
+    csv_writer.writerow(field_data)
+    self.assertEqual(output_buffer.getvalue().strip(), expected_output)
+    
+
+  def test_format_output_line2(self):
+    field_data = ['ABC', None, '4.59', -4300, 'QA', 1.3]
+    expected_output = 'ABC,,4.59,-4300,QA,1.3'
+    output_buffer = io.BytesIO() # Essentially, mocking file output
+    csv_writer = scale_mapper.get_csv_writer(output_buffer)
+    csv_writer.writerow(field_data)
+    self.assertEqual(output_buffer.getvalue().strip(), expected_output)
+
+
+  def test_ScalingData_init1(self):
+    # Test default construction
+    tsd = scale_mapper.ScalingData()
+    self.assertEqual(float(sys.maxint), tsd.get_min())
+    self.assertEqual(float(-sys.maxint), tsd.get_max())
+    self.assertEqual( 0, tsd.scale_text(''))
+    self.assertEqual( 0, tsd.scale_text('ABC'))
+    self.assertEqual( 0, tsd.scale_text(''))
+
+
+  def test_ScalingData_is_numeric1(self):
+    # Test default construction
+    tsd = scale_mapper.ScalingData()
+    self.assertTrue(tsd.is_numeric(0))
+    self.assertTrue(tsd.is_numeric(1.42))
+    self.assertTrue(tsd.is_numeric(-3393939.38383))
+    self.assertTrue(tsd.is_numeric(43e19))
+    self.assertTrue(tsd.is_numeric('123.456'))
+    self.assertTrue(tsd.is_numeric('-18.33e8'))
+
+
+  def test_ScalingData_is_numeric2(self):
+    # Test default construction
+    tsd = scale_mapper.ScalingData()
+    self.assertFalse(tsd.is_numeric(''))
+    self.assertFalse(tsd.is_numeric('AB12'))
+    self.assertFalse(tsd.is_numeric('123ABC'))
+
+
+  def test_ScalingData_scale_type1(self):
+    tsd = scale_mapper.ScalingData()
+    self.assertEqual('U', tsd.get_scale_type())
+
+
+  def test_ScalingData_scale_type2(self):
+    tsd = scale_mapper.ScalingData()
+    tsd.set_scale_type('T')
+    self.assertEqual('T', tsd.get_scale_type())
+    tsd.set_scale_type('N')
+    self.assertEqual('N', tsd.get_scale_type())
+
+
+  def test_ScalingData_scale_type3(self):
+    tsd = scale_mapper.ScalingData()
+    with self.assertRaises(AttributeError):
+      tsd.set_scale_type('U')
+    with self.assertRaises(AttributeError):
+      tsd.set_scale_type('V')
+    with self.assertRaises(AttributeError):
+      tsd.set_scale_type(19)
+    with self.assertRaises(AttributeError):
+      tsd.set_scale_type({0:0})
+
+  def test_ScalingData_minmax1(self):
+    tsd = scale_mapper.ScalingData()
+    tsd.set_scale_type('N')
+    tsd.set_min(9)
+    tsd.set_max(199.0)
+    self.assertEqual(9, tsd.get_min())
+    self.assertEqual(199, tsd.get_max())
+
+
+  def test_ScalingData_scale1(self):
+    tsd = scale_mapper.ScalingData()
+    with self.assertRaises(AttributeError):
+      tsd.scale(1)
+    with self.assertRaises(AttributeError):
+      tsd.scale('A')
+   
+  def test_ScalingData_scale2(self):
+    tsd = scale_mapper.ScalingData()
+    scale_type = 'N'
+    scale_min = 4
+    scale_max = 99
+    value_to_scale = 5
+    expected_result = (float(value_to_scale) - scale_min) / (scale_max - scale_min)
+    
+    tsd.set_scale_type('N')
+    tsd.set_min(scale_min)
+    tsd.set_max(scale_max)
+    self.assertEqual(expected_result, tsd.scale(value_to_scale))
+    
+
+  def test_ScalingData_scale3(self):
+    tsd = scale_mapper.ScalingData()
+    scale_type = 'N'
+    scale_min = float(sys.maxint)
+    scale_max = float(-sys.maxint)
+    value_to_scale = 5
+    expected_result = (float(value_to_scale) - scale_min) / (scale_max - scale_min)
+    
+    tsd.set_scale_type('N')
+    tsd.set_min(scale_min)
+    tsd.set_max(scale_max)
+    self.assertEqual(expected_result, tsd.scale(value_to_scale))
+
+
+  def test_ScalingData_scale4(self):
+    tsd = scale_mapper.ScalingData()
+    scale_type = 'N'
+    scale_min = float(sys.maxint)
+    scale_max = float(-sys.maxint)
+    value_to_scale = 9.9383393939e14
+    expected_result = (float(value_to_scale) - scale_min) / (scale_max - scale_min)
+    
+    tsd.set_scale_type('N')
+    tsd.set_min(scale_min)
+    tsd.set_max(scale_max)
+    self.assertEqual(expected_result, tsd.scale(value_to_scale))
+
+
+    
+#  def test_initialize_range_file_results(self):
+    
 
 
 if __name__ == '__main__':
